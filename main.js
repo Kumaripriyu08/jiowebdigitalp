62c6eef79fb742e813578ea38692365035f79670
Kumaripriyu08 <kumaripriyu08@gmail.com>
2025-07-18 14:56:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// active navber
+let anv = document.querySelector(".navigation-wrap");
+window.onscroll = function (){
+    if(document.documentElement.scrollTop >20){
+        nav.classList.add("scroll-on");
+    } else {
+        nav.classList.add("scroll-on");
+            }
+}
+
+
+// nav hide
+
+let navBar  = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+//counter design
+
+
+
+
